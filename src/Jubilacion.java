783c28f594fc33269567ce9836eb798f395b09be
Juan Jesús Avila Mota <juanmotaavila@gmail.com>
2026-09-25 08:17:10 -0600

    Subir cambios mayoria de edad

--- src/Jubilacion.java
+++ src/Jubilacion.java
@@ -5,6 +5,7 @@ public class Jubilacion { //algoritmo
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         final int EDAD_JUBILACION = 65;
+        final int MAYORIA_DE_EDAD = 18;
         String nombre;
         int edad = 0;
         System.out.println("Escribe tu nombre");
@@ -14,7 +15,11 @@ public class Jubilacion { //algoritmo
         if(edad >= EDAD_JUBILACION) { // SI edad >= 65 entonces....... true o false
             System.out.println(nombre + " Tiene " + edad + " años y esta listo para jubilarse");
         }
+        else if (edad >= 18) { //inicio else-if
+            System.out.printf(nombre+ " Es mayor de edad");
+        } //fin else-if
         else{ //sino
+            System.out.println(nombre + "Es menor de edad");
             System.out.println(nombre + " Tiene " + edad + " años y aún no se puede jubilar");
             System.out.println("Le faltan " + (EDAD_JUBILACION-edad) + " años para jubilarse");
         } //FinSi
